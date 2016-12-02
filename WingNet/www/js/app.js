09f5769dcfc1c6b99b58deef4a368a47ab9b4b08
Jani Timonen <janij.timonen@gmail.com>
2016-12-02 13:15:12 +0200

    fixed cache

--- WingNet/www/js/app.js
+++ WingNet/www/js/app.js
@@ -4,95 +4,99 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module( 'default', [ 'ionic', 'default.controllers' ] )
+angular.module('default', ['ionic', 'default.controllers'])
 
-.run( function( $ionicPlatform ) {
-  $ionicPlatform.ready( function() {
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if ( window.cordova && window.cordova.plugins.Keyboard ) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar( true );
-      cordova.plugins.Keyboard.disableScroll( true );
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
 
     }
-    if ( window.StatusBar ) {
+    if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
-  } );
-} )
+  });
+})
 
-.config( function( $stateProvider, $urlRouterProvider ) {
+.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-    .state( 'app', {
+    .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/menu.html',
     controller: 'AppCtrl'
-  } )
+  })
 
-  .state( 'app.find', {
+  .state('app.find', {
     url: '/find',
+    cache: false,
     views: {
       'menuContent': {
         templateUrl: 'templates/find.html'
       }
     }
-  } )
+  })
 
-  .state( 'app.login', {
+  .state('app.login', {
     url: '/login',
     views: {
       'menuContent': {
         templateUrl: 'templates/login.html'
       }
     }
-  } )
+  })
 
-  .state( 'app.register', {
+  .state('app.register', {
     url: '/register',
+    cache: false,
     views: {
       'menuContent': {
         templateUrl: 'templates/register.html'
       }
     }
-  } )
+  })
 
-  .state( 'app.welcome', {
+  .state('app.welcome', {
     url: '/welcome',
+    cache: false,
     views: {
       'menuContent': {
         templateUrl: 'templates/welcome.html'
       }
     }
-  } )
+  })
 
-  .state( 'app.requests', {
+  .state('app.requests', {
       url: '/requests',
+      cache: false,
       views: {
         'menuContent': {
           templateUrl: 'templates/requests.html'
         }
       }
-    } )
-    .state( 'app.profile', {
+    })
+    .state('app.profile', {
       url: '/profile',
       views: {
         'menuContent': {
           templateUrl: 'templates/profile.html'
         }
       }
-    } )
-    .state( 'app.about', {
+    })
+    .state('app.about', {
       url: '/about',
       views: {
         'menuContent': {
           templateUrl: 'templates/about.html'
         }
       }
-    } );
+    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise( '/app/login' );
-} );
+  $urlRouterProvider.otherwise('/app/login');
+});
