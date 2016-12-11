896c28ccbef75ab2fe72783fa1b4c4e6356b3a37
Juho Lahtinen <juholahtinen66@gmail.com>
2016-12-11 14:44:03 +0200

    Message interval done

--- WingNet/www/js/controllers/messageController.js
+++ WingNet/www/js/controllers/messageController.js
@@ -2,15 +2,7 @@ angular.module('default.controllers').controller('MessageController',
   function($scope, Connection, $state, $interval) {
     conversationId = "584d377256776e2d1bc87060";
 
-    Connection.getMessages(conversationId)
-      .success(function(result) {
-        if (result.success) {
-          $scope.messages = result.data;
-          console.log(JSON.stringify(result.data));
-        } else {
-          alert(result.message);
-        }
-      });
+
 
     $scope.sendMessage = function() {
       var message = $scope.messenger.message;
@@ -37,4 +29,21 @@ angular.module('default.controllers').controller('MessageController',
       angular.element("#sendMessageButton").show();
     }
 
+    var interval = $interval(callAtInterval, 5000);
+
+    function callAtInterval() {
+      Connection.getMessages(conversationId)
+        .success(function(result) {
+          if (result.success) {
+            $scope.messages = result.data;
+            console.log(JSON.stringify(result.data));
+          } else {
+            alert(result.message);
+          }
+        });
+    }
+
+    $scope.$on('$destroy', function() {
+      $interval.cancel(interval);
+    });
   });
